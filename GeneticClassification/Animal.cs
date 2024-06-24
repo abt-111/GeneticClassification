ecd654845f6be68897c85e0019b8651aeb4dbbf8
abt-111 <anthony.temmel@gmail.com>
2024-06-24 11:44:16 +0200

    Ajout de la classe abstraite Animal

---
+++ GeneticClassification/Animal.cs
@@ -0,0 +1,16 @@
+﻿namespace GeneticClassification
+{
+    internal class Animal
+    {
+        protected int legCount;
+        public virtual void Move()
+        {
+            Console.WriteLine("Un pattes après l'autre");
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine("Mettre les aliments dans sa bouche et mâcher");
+        }
+    }
+}
